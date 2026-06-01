7343dca4b20d2ceaf50f3e4c1ff8fa5f7d095984
L0renz008 <lrenouvin98@gmail.com>
2026-06-01 15:57:41 +0200

    fix: remove unused useEffect import in HomeClient component

--- components/HomeClient.tsx
+++ components/HomeClient.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Session } from "@/types";
 import Header from "./Header";
 import WeekStrip from "./WeekStrip";
